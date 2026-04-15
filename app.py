b0c3257d7634ae637f66db11342b1dd444dadee7
Abhishek H N <1NC25MC001@ncetmail.com>
2026-04-15 11:32:28 +0530

    Fix formatting and update content generation logic

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ from groq import Groq
 st.set_page.contig("StartZen Content Generator",layout="wide")
 st.title("StartZenAI - Content Generator")
 st.image("download1.jpg")
-client =  Groq(api_key = st.secrets["GROQ_API_KEY"])
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 product = st.text_input("Product")
 audience = st.text_input("Audience")
 if st.button("Generate Content"):
-    prompt = f"write marketing content for (product) targeting (audience)."
-response =  Client.chat.completions.create(
-  model = "llama-3.3-70b-versatile",
-  messages=[{"role": "user" , "content": prompt}]
-)
-st.session_stste.text = response.choices[0].message.content
-text =response.choices[0].message.content
-st.write(text)
+    prompt = f"Write marketing content for {product} targeting {audience}."
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    st.session_state.text = response.choices[0].message.content
+    text =response.choices[0].message.content
+    st.write(text)
 if "text" in st.session_state:
-   content = st.text_area("Generated Content", st.session_state.text, height=300)
-   st.download_button(
-       lable="⬇️ Download as TXT",
-       data=content,
-       file_name="marketing_copy.txt",
-       mime="text/plain"
-  )
-else:
-  st.info("Generate Content First")
+    content = st.text_area("Generated Content", st.session_state.text, height=300)
+    st.download_button(
+            label="⬇️ Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+        )
+    else:
+        st.info("Generate content first")
